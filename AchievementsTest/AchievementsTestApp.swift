553cdc6bf7db06a827e29200bd1c3dffa9154ea6
vince <vromani@outlook.com>
2020-12-18 12:49:02 -0500

    Initial Commit

---
+++ AchievementsTest/AchievementsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AchievementsTestApp.swift
+//  AchievementsTest
+//
+//  Created by Vince on 2020-12-18.
+//
+
+import SwiftUI
+
+@main
+struct AchievementsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
